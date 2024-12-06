a22d9ac5f75c3c29bb7cad0df0f1b14db21beb1a
VictorBaron1 <victorb104@nycstudents.net>
2024-12-06 14:33:30 +0000

    final done

--- Main.java
+++ Main.java
@@ -9,7 +9,11 @@ public class Main
         for(int i = 50; i < 60; i++) schedule[1][i] = true;
         a.printPeriod(2);
         a.findFreeBlock(2, 15);
+        a.findFreeBlock(2, 9);
+        a.findFreeBlock(2, 20);
         System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 9));
+        System.out.println(a.findFreeBlock(2, 20));
         schedule = new boolean[8][60];
         for(int i= 25; i < 30; i++)schedule[1][i] = true;
         for(int i= 0; i < 15; i++)schedule[2][i] = true;
@@ -18,14 +22,20 @@ public class Main
         for(int i= 44; i < 60; i++)schedule[3][i] = true;
         AppointmentBook b = new AppointmentBook(schedule);
         int period = 2;
-        // while(period < 5)
-        // {
-        //     System.out.println("Period: " + period);
-        //     b.printPeriod(period);
-        //     period++;
-        // }
+        while(period < 5)
+        {
+             System.out.println("Period: " + period);
+             b.printPeriod(period);
+             period++;
+         }
         b.printPeriod(4);
         System.out.println(b.makeAppointment(2, 4, 22));
+        b.printPeriod(4);
+        b.printPeriod(3);
+        System.out.println(b.makeAppointment(3, 4, 3));
+        b.printPeriod(3);
+        b.printPeriod(4);
+        System.out.println(b.makeAppointment(2, 4, 30));
         b.printPeriod(4);
     }
 }
